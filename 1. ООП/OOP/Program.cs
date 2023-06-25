03571b4bfbed2831407d67abc715de908b32e419
kabanoff-i <iliakabanov1807@gmail.com>
2023-06-25 12:59:00 +0300

    OOP, Value type reference type, type conversion completed

---
+++ 1. ООП/OOP/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace OOP
+{
+    public abstract class Car
+    {
+        public string make { get; set; }
+        public string model { get; set; }
+        public abstract void ChangeGear();
+        public Car(string make, string model)
+        {
+            this.make = make;
+            this.model = model;
+        }
+    }
+    public class ManualTransmission : Car
+    {
+        private static int n = 0;
+        private static string[] gears = new string[] { "N", "1", "2", "3", "4", "5", "R" };
+        private string gear = gears[n];
+        private void SqueezeClutch()
+        {
+            Console.WriteLine("Clutch squeezed");
+
+        }
+        public override void ChangeGear()
+        {
+            SqueezeClutch();
+            gear = gears[(n + 1) % gears.Length];
+            Console.WriteLine("The gear was changed, current gear: " + gear);
+        }
+        public ManualTransmission(string make, string model) : base(make, model)
+        {
+        }
+
+    }
+    public class AutomaticTransmission : Car
+    {
+        private static int n = 0;
+        private static string[] gears = new string[] { "N", "D", "P", "R" };
+        private string gear = gears[n];
+        public override void ChangeGear()
+        {
+            gear = gears[(n + 1) % gears.Length];
+            Console.WriteLine("The gear was changed, current gear: " + gear);
+        }
+        public AutomaticTransmission(string make, string model) : base(make, model)
+        {
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = new Car[] { new ManualTransmission("Honda", "Civic"), new AutomaticTransmission("Lexus", "RX450h") };
+            foreach (var car in cars)
+            {
+                car.ChangeGear();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Application/Models/Models.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Models
+{
+    public abstract class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Person(string name, string surname, DateTime date)
+        {
+            Name = name;
+            Surname = surname;
+            DateOfBirth = date;
+        }
+    }
+    public class Employee : Person
+    {
+        public Employee(string name, string surname, DateTime date, string contract, string jobTitle, int salary, DateTime dateOfHire) : base(name, surname, date)
+        {
+            Contract = contract;
+            JobTitle = jobTitle;
+            Salary = salary;
+            DateOfHire = dateOfHire;
+        }
+
+        public string Contract { get; set; }
+        public string JobTitle { get; set; }
+        public int Salary { get; set; }
+        public DateTime DateOfHire { get; set; }
+    }
+    public class Client : Person
+    {
+        public Client(string name, string surname, DateTime date, string clientID, string phoneNumber, string email) : base(name, surname, date)
+        {
+            ClientID = clientID;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public string ClientID { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+    public struct Currency
+    {
+        public string name;
+        public string symbol;
+        public string code;
+        public string country;
+        public double exchangeRateToUSD;
+        public Currency(string name, string symbol, string code, string country, double exchangeRateToUSD)
+        {
+            this.name = name;
+            this.symbol = symbol;
+            this.code = code;
+            this.country = country;
+            this.exchangeRateToUSD = exchangeRateToUSD;
+        }
+    }
+}
