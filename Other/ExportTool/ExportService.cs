945c75a29868ae1042a319ae2066a966860a0055
kabanoff-i <iliakabanov1807@gmail.com>
2023-07-31 23:08:50 +0300

    FileStream practice completed

---
+++ Other/ExportTool/ExportService.cs
@@ -0,0 +1,67 @@
+﻿using Models;
+using CsvHelper;
+using System.Globalization;
+
+namespace ExportTool
+{
+    public class ExportService
+    {
+        private string _pathToDirectory { get; set; }
+        private string _csvFileName { get; set; }
+        public ExportService(string pathToDirectory, string csvFileName)
+        {
+            _pathToDirectory = pathToDirectory;
+            _csvFileName = csvFileName;
+        }
+
+        public void ImportDBToCSV(List<Client> clients)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(_pathToDirectory);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            string fullPath = GetFullPathToFile(_pathToDirectory, _csvFileName);
+            using (FileStream fileStream = new FileStream(fullPath, FileMode.OpenOrCreate))
+            {
+                using (StreamWriter streamWriter = new StreamWriter(fileStream))
+                {
+                    using (var writer = new CsvWriter(streamWriter, CultureInfo.InvariantCulture))
+                    {
+                        writer.WriteRecords(clients);
+                    }
+                }
+            }
+        }
+        //if it is necessary to change the csv file
+        public List<Client> ImportCSVToDB(string pathToDirectory, string csvFileName)
+        {
+            _pathToDirectory = pathToDirectory;
+            _csvFileName = csvFileName;
+            return ImportCSVToDB();
+        }
+        public List<Client> ImportCSVToDB()
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(_pathToDirectory);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            string fullPath = GetFullPathToFile(_pathToDirectory, _csvFileName);
+            using (FileStream fileStream = new FileStream(fullPath, FileMode.OpenOrCreate))
+            {
+                using (StreamReader streamReader = new StreamReader(fileStream))
+                {
+                    using (var reader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
+                    {
+                        return (List<Client>) reader.GetRecords<Client>();
+                    }
+                }
+            }
+        }
+        private string GetFullPathToFile(string pathToFile, string fileName)
+        {
+            return Path.Combine(pathToFile, fileName);
+        }
+    }
+}
