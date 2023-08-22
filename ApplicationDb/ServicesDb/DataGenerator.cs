c0b59ccf37baf08ebea9739b255a995e16cbcbc5
kabanoff-i <iliakabanov1807@gmail.com>
2023-08-22 21:27:33 +0300

    Serialization practice completed

--- ApplicationDb/ServicesDb/DataGenerator.cs
+++ ApplicationDb/ServicesDb/DataGenerator.cs
@@ -14,5 +14,9 @@ namespace ServicesDb
             var person = new Bogus.Person();
             return new Employee(person.FirstName, person.LastName, person.DateOfBirth, Guid.NewGuid(), person.Company.Name, person.Company.Name, new Random().Next(3000), person.DateOfBirth);
         }
+        public static Account GetAccount(Client client, Currency currency)
+        {
+            return new Account(Guid.NewGuid(), currency.code, client.id, 0, client, currency);
+        }
     }
 }
--- Other/ExportTool/ExportService.cs
+++ Other/ExportTool/ExportService.cs
@@ -1,16 +1,17 @@
-﻿    using CsvHelper;
+﻿using CsvHelper;
 using System.Globalization;
+using Newtonsoft.Json;
 
 namespace ExportTool
 {
     public class ExportService
     {
         private string _pathToDirectory { get; set; }
-        private string _csvFileName { get; set; }
-        public ExportService(string pathToDirectory, string csvFileName)
+        private string _fileName { get; set; }
+        public ExportService(string pathToDirectory, string fileName)
         {
             _pathToDirectory = pathToDirectory;
-            _csvFileName = csvFileName;
+            _fileName = fileName;
         }
 
         public void ImportDBToCSV<T>(IEnumerable<T> persons)
@@ -20,7 +21,7 @@ namespace ExportTool
             {
                 dirInfo.Create();
             }
-            string fullPath = GetFullPathToFile(_pathToDirectory, _csvFileName);
+            string fullPath = GetFullPathToFile(_pathToDirectory, _fileName);
             using (FileStream fileStream = new FileStream(fullPath, FileMode.OpenOrCreate))
             {
                 using (StreamWriter streamWriter = new StreamWriter(fileStream))
@@ -36,7 +37,7 @@ namespace ExportTool
         public IEnumerable<T> ImportCSVToDB<T>(string pathToDirectory, string csvFileName)
         {
             _pathToDirectory = pathToDirectory;
-            _csvFileName = csvFileName;
+            _fileName = csvFileName;
             return ImportCSVToDB<T>();
         }
         public List<T> ImportCSVToDB<T>()
@@ -46,7 +47,7 @@ namespace ExportTool
             {
                 dirInfo.Create();
             }
-            string fullPath = GetFullPathToFile(_pathToDirectory, _csvFileName);
+            string fullPath = GetFullPathToFile(_pathToDirectory, _fileName);
             using (FileStream fileStream = new FileStream(fullPath, FileMode.OpenOrCreate))
             {
                 using (StreamReader streamReader = new StreamReader(fileStream))
@@ -61,6 +62,42 @@ namespace ExportTool
         private string GetFullPathToFile(string pathToFile, string fileName)
         {
             return Path.Combine(pathToFile, fileName);
+        }
+        //Json serialized
+        public async Task ExportDBToJson<T>(IEnumerable<T> persons)
+        {
+            string json = JsonConvert.SerializeObject(persons, Formatting.Indented, new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore});
+            string fullPath = GetFullPathToFile(_pathToDirectory, _fileName);
+
+            using (FileStream fileStream = new FileStream(fullPath, FileMode.Create))
+            {
+                using (StreamWriter streamWriter = new(fileStream))
+                {
+                    await streamWriter.WriteAsync(json);
+                }
+            }
+        }
+        public async Task<List<T>> ImportJsonToDB<T>(string pathDirectory, string fileName)
+        {
+            _pathToDirectory = pathDirectory;
+            _fileName = fileName;
+            return await ImportJsonToDB<T>();
+        }
+
+        public async Task<List<T>> ImportJsonToDB<T>()
+        {
+            string fullPath = GetFullPathToFile(_pathToDirectory, _fileName);
+
+            using (FileStream fileStream = new FileStream(fullPath, FileMode.Open))
+            {
+                using (StreamReader streamReader = new(fileStream))
+                {
+                    string json = await streamReader.ReadToEndAsync();
+                    List<T> persons = JsonConvert.DeserializeObject<List<T>>(json);
+                    return persons;
+                }
+            }
+
         }
     }
 }
